f2fd99be4b317b7ef3e1c65819b9597fa4487a9e
Mathias Punkenhofer <mpunkenhofer@gmail.com>
2020-02-19 18:18:02 +0100

    feat: deleting tags by dragging them in a remove zone

--- src/features/TagList/TagList.tsx
+++ src/features/TagList/TagList.tsx
@@ -2,12 +2,11 @@ import * as React from 'react';
 import {SortableContainer, SortableElement} from 'react-sortable-hoc';
 import arrayMove from 'array-move';
 import {useDispatch, useSelector} from 'react-redux'
-import {useEffect} from "react";
-import {getUser, updateTags} from "../../slices/user";
+import {useEffect, useState} from "react";
+import {getUser, removeTag, updateTags} from "../../slices/user";
 import {RootState} from "../../app/rootReducer";
 import Tag from "../../components/Tag";
 import {Tag as TagType} from "../../api/storageAPI"
-import isEmpty from 'lodash/isempty';
 
 interface SortableItemProps {
     tag: TagType
@@ -16,13 +15,23 @@ interface SortableItemProps {
 const SortableItem = SortableElement(({tag}: SortableItemProps) => <li><Tag tag={tag}/></li>);
 
 interface SortableListProps {
-    tags: TagType[]
-
+    tags: TagType[],
+    removeZoneVisible: boolean,
+    setHoveringRemoveZone: (boolean) => void,
 }
-const SortableList = SortableContainer(({tags}: SortableListProps) =>
-    <ul>
-        {tags.map((tag, index) => (<SortableItem key={`item-${tag.id}`} index={index} tag={tag}/>))}
-    </ul>
+
+const SortableList = SortableContainer(({tags,
+                                            removeZoneVisible, setHoveringRemoveZone}: SortableListProps) =>
+    <>
+        <ul>
+            {tags.map((tag, index) => (<SortableItem key={`item-${tag.id}`} index={index} tag={tag}/>))}
+        </ul>
+        {
+            removeZoneVisible && <div className={'lt-list-remove-zone'}
+                                      onMouseEnter={() => setHoveringRemoveZone(true)}
+                                      onMouseLeave={() => setHoveringRemoveZone(false)}/>
+        }
+    </>
 );
 
 interface TagListProps {
@@ -31,7 +40,12 @@ interface TagListProps {
 
 const TagList = ({username}: TagListProps) => {
     const dispatch = useDispatch();
-    const {user, loading, error} = useSelector((state: RootState) =>
+
+    const [tags, setTags] = useState([]);
+    const [removeZoneVisible, setRemoveZoneVisible] = useState(false);
+    const [hoveringRemoveZone, setHoveringRemoveZone] = useState(false);
+
+    const {user, error} = useSelector((state: RootState) =>
         state.user.userRecord[username] ? state.user.userRecord[username] : {user: null, loading: false, error: null}
     );
     const {tagsById} = useSelector((state: RootState) => state.sets);
@@ -41,24 +55,50 @@ const TagList = ({username}: TagListProps) => {
         dispatch(getUser(username));
     }, [username, dispatch]);
 
+
+    useEffect(() => {
+        if (user && user.tags && tagsById) {
+            setTags(user.tags.map(tagId => (tagsById[tagId])).filter(tag => (tag !== undefined)));
+        }
+    }, [user, tagsById]);
+
+    const onSortStart = () => {
+        setRemoveZoneVisible(true);
+    };
+
     const onSortEnd = ({oldIndex, newIndex}) => {
-        if(user.tags)
-            dispatch(updateTags(username, arrayMove([...user.tags], oldIndex, newIndex)));
+        setRemoveZoneVisible(false);
+        setHoveringRemoveZone(false);
+
+        if(hoveringRemoveZone) {
+            setTags(tags.splice(oldIndex, 1));
+            if(user.tags && user.tags.length > oldIndex)
+                dispatch(removeTag(username, user.tags[oldIndex]));
+        } else {
+            setTags(arrayMove(tags, oldIndex, newIndex));
+            if (user.tags)
+                dispatch(updateTags(username, arrayMove([...user.tags], oldIndex, newIndex)));
+        }
     };
 
     if (error) {
         console.error(error);
         return null;
-    } else if (!loading && user && user.tags.length > 0 && !isEmpty(tagsById)) {
-        return (
-                <SortableList tags={user.tags.map(tagId => tagsById[tagId] ? tagsById[tagId] : null)}
-                              onSortEnd={onSortEnd}
-                              axis="x" lockAxis="x"
-                              lockToContainerEdges={true}/>
-        );
-    } else {
+    }
+
+    if (tags.length < 1) {
         return null;
     }
+
+    return (
+        <SortableList tags={tags}
+                      removeZoneVisible={removeZoneVisible}
+                      setHoveringRemoveZone={setHoveringRemoveZone}
+                      onSortStart={onSortStart}
+                      onSortEnd={onSortEnd}
+                      axis="x" lockAxis="x"
+                      lockToContainerEdges={true}/>
+    );
 };
 
 export default TagList;
