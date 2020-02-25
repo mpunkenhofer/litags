bf994cdc351e5ba2e1ad412ef93d5800053e6789
Mathias Punkenhofer <mpunkenhofer@gmail.com>
2020-02-25 10:35:16 +0100

    chore: use react portals instead of multiple react renders

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,23 +2,48 @@ import * as React from "react";
 import * as ReactDOM from 'react-dom';
 import TagList from "../components/TagList/TagList";
 import TagChooser from "../components/TagChooser/TagChooser";
+import {useEffect, useState} from "react";
 
-interface AppData {
-    username: string;
-    anchor: HTMLElement
+interface Mount {
+    username: string,
+    mount: HTMLElement,
+    keyboardShortcutsEnabled?: boolean
 }
 
 interface AppProps {
-    anchors: AppData[]
+    mounts: Mount[]
 }
 
-export const App = ({anchors}: AppProps) => {
+const Container = ({username, mount, keyboardShortcutsEnabled}: Mount) => {
+    const [listElement] = useState(document.createElement('div'));
+    const [buttonElement] = useState(document.createElement('div'));
+
+    listElement.classList.add('lt-list');
+    buttonElement.classList.add('lt-button');
+
+    useEffect(() => {
+        mount.appendChild(listElement);
+        mount.appendChild(buttonElement);
+
+        return () => {
+            mount.removeChild(listElement);
+            mount.removeChild(buttonElement);
+        }
+    }, []);
+
     return (
         <>
-            {anchors.map(data => {
-                ReactDOM.createPortal(<TagList username={data.username}/>, data.anchor);
-                ReactDOM.createPortal(<TagChooser username={data.username}/>, data.anchor);
-            })}
+            {ReactDOM.createPortal(<TagList username={username}/>, listElement)}
+            {ReactDOM.createPortal(<TagChooser username={username}
+                                               keyboardShortcutsEnabled={keyboardShortcutsEnabled}/>, buttonElement)}
+        </>
+    );
+};
+
+export const App = ({mounts}: AppProps) => {
+    return (
+        <>
+            {mounts.map(data => (<Container username={data.username} mount={data.mount}/>))}
         </>
     );
 };
--- src/content.tsx
+++ src/content.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import * as ReactDOM from 'react-dom';
-import TagList from "./components/TagList/TagList";
-import TagChooser from "./components/TagChooser/TagChooser";
 import {Provider} from 'react-redux'
 import store from './app/store'
 import {enableStorageApiLogger} from "./api/storageAPI";
+import {App} from "./app/App";
 
 console.log('LiTags is open source! https://github.com/mpunkenhofer/litags');
 
-if(process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
     enableStorageApiLogger();
 }
+
+const getLiTagsRoot = () => {
+    const root = document.getElementById('litags');
+    if (root)
+        return root;
+    else {
+        const newRoot = document.createElement('div');
+        newRoot.id = 'litags';
+        document.body.append(newRoot);
+        return newRoot;
+    }
+};
 
 const element = document.querySelector('.round__app');
 
@@ -18,13 +29,24 @@ if (element) {
     new MutationObserver((mutations, observerInstance) => {
         observerInstance.disconnect();
 
-        createLiTagsElements(document.querySelector('.ruser-top'),
-            getUserName('.ruser-top a'), true);
-        createLiTagsElements(document.querySelector('.ruser-bottom'),
-            getUserName('.ruser-bottom a'));
+        ReactDOM.render(
+            <Provider store={store}>
+                <App mounts={[
+                    {
+                        username: getUserName('.ruser-top a'),
+                        mount: document.querySelector('.ruser-top'),
+                        keyboardShortcutsEnabled: true
+                    },
+                    {
+                        username: getUserName('.ruser-bottom a'),
+                        mount: document.querySelector('.ruser-bottom')
+                    }
+                ]}/>
+            </Provider>, getLiTagsRoot()
+        );
     }).observe(element, {childList: true, attributes: true, subtree: true, characterData: true});
 } else {
-    console.log('LiTags found no supported anchors on this page.');
+    console.log('LiTags found no supported mounts on this page.');
 }
 
 const getUserName = (elementName: string) => {
@@ -34,29 +56,4 @@ const getUserName = (elementName: string) => {
 const removeTitle = (name: string) => {
     const s = name.split(/(\s+)/);
     return s ? s[s.length - 1] : '';
-};
-
-const createLiTagsElements = (anchor: HTMLElement, username: string, keyboardShortcutsEnabled: boolean = false) => {
-    if (!anchor || !username)
-        return;
-
-    const listElement = document.createElement('div');
-    listElement.className = 'lt-list';
-    const buttonElement = document.createElement('div');
-    buttonElement.className = 'lt-button';
-
-    anchor.append(listElement);
-    anchor.append(buttonElement);
-
-    ReactDOM.render(
-        <Provider store={store}>
-            <TagList username={username}/>
-        </Provider>,
-        listElement);
-
-    ReactDOM.render(
-        <Provider store={store}>
-            <TagChooser username={username} keyboardShortcutsEnabled={keyboardShortcutsEnabled}/>
-        </Provider>,
-        buttonElement);
 };
